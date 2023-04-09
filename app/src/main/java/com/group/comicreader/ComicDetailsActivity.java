852aee6ad424d2faba3bfdffae1a894277abdec3
Tri <ngminhtri002@gmail.com>
2023-04-09 17:09:16 +0700

    Add tool bar for comic details view. Center details in respect to comic cover.

--- app/src/main/java/com/group/comicreader/ComicDetailsActivity.java
+++ app/src/main/java/com/group/comicreader/ComicDetailsActivity.java
@@ -1,17 +1,23 @@
 package com.group.comicreader;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.group.comicreader.adapters.ChapterListAdapter;
 import com.group.comicreader.models.Chapter;
 import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,15 +31,32 @@ public class ComicDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comic_details);
 
+        // Set up toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar_details);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        // Show back button and handle its click event
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
         // Find views
         TextView titleTextView = findViewById(R.id.text_details_title);
+        TextView authorTextView = findViewById(R.id.text_details_author);
+        TextView genresTextView = findViewById(R.id.text_details_genres);
         TextView descriptionTextView = findViewById(R.id.text_details_description);
         ImageView coverImageView = findViewById(R.id.image_details_cover);
         chaptersRecyclerView = findViewById(R.id.recycler_details_chapters);
 
         // Set comic details
-        titleTextView.setText("Comic Title");
-        descriptionTextView.setText("Description");
+        titleTextView.setText("Chainsaw Man");
+        authorTextView.setText("FUJIMOTO Tatsuki");
+        genresTextView.setText("Action, Comedy, Drama, Horror, Mature, Shounen, Supernatural");
+        descriptionTextView.setText("Meet Denji, he's poor. Like, sell your body parts poor. But he's found a gig killing demons with his chainsaw dog. Will it be enough to pay the bills?");
         Picasso.get().load(R.drawable.chainsaw_man).into(coverImageView);
 
         // Add sample chapters
